33e3ca90bc73b1b601de34eb2ce810d3125f3c62
Topher Hunt <hunt.topher@gmail.com>
2020-10-13 10:41:34 +0200

    Log each email sent

--- lib/vanilla/mailer.ex
+++ lib/vanilla/mailer.ex
@@ -5,11 +5,21 @@ defmodule Vanilla.Mailer do
   def send(email) do
     try do
       deliver_now(email)
+      log :info, "Sent #{describe_email(email)}"
       {:ok}
     # SMTP failures may raise ErlangErrors which need to be caught rather than rescued (?)
     catch e ->
-      Logger.warn "Error sending email #{inspect(email.subject)} to #{inspect(email.to)}: #{inspect(e)}"
+      log :warn, "Error sending email #{describe_email(email)}: #{inspect(e)}"
       {:error, e}
     end
   end
+
+  defp describe_email(email) do
+    module = "#{email.private.view_module}" |> String.split(".") |> List.last()
+    template = email.private.view_template |> String.split(".") |> List.first()
+    to = inspect(email.to)
+    "to: #{to}, template: #{module}.#{template}, subject: \"#{email.subject}\")"
+  end
+
+  defp log(level, msg), do: Logger.log(level, "Worldviews.Mailer: #{msg}")
 end
